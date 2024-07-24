bc9f5eb40c4d4d514b4bebcd12ad6605c4b511bb
12Ahn22 <12ayo22@gmail.com>
2024-07-24 15:00:06 +0900

    style: 다른 폰트 추가하기

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,12 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana, Noto_Sans_KR  } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const notoSans = Noto_Sans_KR({
+  subsets:[]
+})
